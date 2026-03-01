6decfd2415749ff7b5e5a5b8bc31b54ab98ede41
Aryan Sanojraj <147275106+SJAryan@users.noreply.github.com>
2026-02-28 19:31:33 -0600

    addEmail

--- backend/db_app.py
+++ backend/db_app.py
@@ -1,5 +1,5 @@
 import os 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from pymongo import MongoClient
 from dotenv import load_dotenv
 
@@ -11,16 +11,24 @@ try:
     client = MongoClient(MONGO_URI)
     db = client.get_database()
     client.admin.command('ping') 
-    print("Connected to MongoDP and pinged")
+    print("Connected to MongoDB and pinged")
 except Exception as e: 
     print(f"Error connecting to MongoDB: {e}")  
 
 
-app = Flask(__name__) 
 
-@app.route("/get_all", methods=["GET"]) 
+@app.route("/", methods=["GET"]) 
 def home():
-    return jsonify({"message": "Welcome to the TagIt API!"}) 
+    return jsonify({"message": "Welcome to the TagIt API!"})  
+
+@app.route("/api/emails", methods=["POST"])
+def add_email():
+    incoming_data = request.json
+    result = db.emails.insert_one(incoming_data)
+    return jsonify({
+        "message": "Email successfully saved to the filing cabinet!", 
+        "id": str(result.inserted_id)
+    }), 201
 
 
 if __name__ == "__main__":
